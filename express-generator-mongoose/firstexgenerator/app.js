31bca2bc9ad778ad36ac598cccedc3fa8a867026
shamaz saeed <shamazsaeed332@gmail.com>
2019-11-03 17:51:14 +0500

    user authentication with passport

--- express-generator-mongoose/firstexgenerator/app.js
+++ express-generator-mongoose/firstexgenerator/app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+//file store will save info about auth making a folder na,ed session
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport')
+var authenticate = require('./authenticate')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -28,51 +35,36 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req, res, next) {
-
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      res.cookie('user', 'admin', { signed: true });
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
-  }
-  else {
-    if (req.signedCookies.user === 'admin') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
-    }
-  }
-}
-
-
-app.use(auth);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+  else {
+      next();
+    }
+}
+
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/dishes', dishRouter);
